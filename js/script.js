d61e1b8b37693771677619cf6d4f7b46cb7558fd
Mark Destriza <mmjdestriza@gmail.com>
2022-08-31 16:40:21 +0800

    fixed sidebar and background

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ window.addEventListener('scroll', function () {
           nav.classList.remove('bg-custom','shadow');
     }
 });
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
