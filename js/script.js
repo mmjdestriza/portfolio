61e7494ceadb636d814a60f7bb11cc6d0f97829c
Mark Destriza <mmjdestriza@gmail.com>
2022-08-31 19:36:30 +0800

    added mobile interface

--- js/script.js
+++ js/script.js
@@ -2,14 +2,6 @@
 var nav = document.querySelector('nav');
 var banner = document.getElementById('banner');
 
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 100) {
-          nav.classList.add('bg-custom','shadow');
-    } else {
-          nav.classList.remove('bg-custom','shadow');
-    }
-});
-
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
